de83f6b2acb5f1ef263caed57120d566865c94f1
btahir89 <bilal.tahir89@gmail.com>
2021-06-02 21:47:05 -0700

    unnecessary div

--- components/HeroContent.js
+++ components/HeroContent.js
@@ -3,19 +3,17 @@ import Subscribe from '../components/Subscribe'
 function HeroContent() {
   return (
     <div className='absolute inset-0 flex justify-center items-center'>
-      <div className='w-full max-w-md backdrop-filter backdrop-blur-md backdrop-grayscale backdrop-brightness-50 sm:rounded-2xl'>
-        <div className='w-full h-full text-white py-8 px-6'>
-          <h1 className='text-center text-3xl sm:text-4xl font-extrabold sm:leading-[3rem]'>
-            Your headline here.{' '}
-            <span className='text-yellow-400'>Catch their attention!</span>
-          </h1>
-          <p className='mt-6 text-lg sm:text-xl'>
-            Ea esse minim reprehenderit voluptate. Ea nisi culpa magna proident
-            id voluptate. Aute labore id aliquip nisi amet voluptate sunt duis
-            proident ea aute fugiat ea. Anim deserunt amet eiusmod ullamco esse.
-          </p>
-          <Subscribe />
-        </div>
+      <div className='w-full max-w-md backdrop-filter backdrop-blur-md backdrop-grayscale backdrop-brightness-50 sm:rounded-2xl text-white py-12 px-4 sm:px-8'>
+        <h1 className='text-center text-3xl sm:text-4xl font-extrabold sm:leading-[3rem]'>
+          Your headline here.{' '}
+          <span className='text-yellow-400'>Catch their attention!</span>
+        </h1>
+        <p className='mt-6 text-lg sm:text-xl'>
+          Ea esse minim reprehenderit voluptate. Ea nisi culpa magna proident id
+          voluptate. Aute labore id aliquip nisi amet voluptate sunt duis
+          proident ea aute fugiat ea. Anim deserunt amet eiusmod ullamco esse.
+        </p>
+        <Subscribe />
       </div>
     </div>
   )
