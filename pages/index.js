74d044f5d3266867c9d5f92039ef88b5b9372114
btahir89 <bilal.tahir89@gmail.com>
2021-06-02 19:26:45 -0700

    snazzier headline

--- pages/index.js
+++ pages/index.js
@@ -12,14 +12,14 @@ export default function Home() {
         <img
           src='https://source.unsplash.com/random'
           alt='main background image'
-          className='absolute inset-0 w-full h-full object-cover filter brightness-90'
+          className='absolute inset-0 w-full h-full object-cover'
         />
 
         <div className='absolute inset-0 flex justify-center items-center'>
           <div className='w-full h-96 max-w-md backdrop-filter backdrop-blur-md backdrop-grayscale backdrop-brightness-50 sm:rounded-2xl'>
             <div className='w-full h-full text-white py-8 px-6'>
-              <h1 className='text-center text-3xl sm:text-4xl font-extrabold'>
-                Your headline here
+              <h1 className='text-center text-3xl sm:text-4xl font-extrabold sm:leading-[3rem]'>
+                Your headline here. <span className="text-yellow-400">Catch their attention!</span>
               </h1>
               <p className='mt-6 text-lg sm:text-xl'>
                 Ea esse minim reprehenderit voluptate. Ea nisi culpa magna
