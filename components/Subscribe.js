f810ce7e17449c9b5809df24c8f1d607849cc622
btahir89 <bilal.tahir89@gmail.com>
2021-06-02 22:00:04 -0700

    placeholder

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -5,12 +5,13 @@ function Subscribe() {
     <form className='mt-8 w-full flex'>
       <input
         className='border rounded-tl rounded-bl border-gray-100 w-full px-4 py-2 text-gray-900'
+        placeholder='Your Email Here'
         type='email'
         required
       />
       <button
         type='submit'
-        className='bg-yellow-400 px-4 py-2 font-medium sm:text-lg rounded-tr rounded-br'
+        className='bg-yellow-400 text-gray-900 px-4 py-2 font-medium sm:text-lg rounded-tr rounded-br'        
       >
         Subscribe
       </button>
